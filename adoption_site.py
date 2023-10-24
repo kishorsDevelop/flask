4705fda8ceb7c5dd8b8717fb02673fa9e3a89b8f
Kishor Kumar <mr.kumar18399@gmail.com>
2023-10-24 18:30:46 +0530

    views

--- adoption_site.py
+++ adoption_site.py
@@ -8,10 +8,9 @@ base_dir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my_secret_key'
-
-db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(base_dir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 Migrate(app, db)
 
@@ -30,6 +29,11 @@ class Puppy(db.Model):
         return f"Puppy name is {self.name}"
 
 ### View Functions ###
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
 
 @app.route('/list')
 def list():
